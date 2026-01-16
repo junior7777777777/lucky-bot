00864311ff24ac6100edec5368c3c1598df5d370
junior7777777777 <fazopix1214@gmail.com>
2026-01-15 23:14:35 -0300

    Add aceitar command to delete channel for staff

--- src/commands/aceitar.js
+++ src/commands/aceitar.js
@@ -1,1 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { isStaff } = require('../utils/permissoes');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('aceitar')
+    .setDescription('Aceitar participante'),
+  async execute(interaction) {
+    if (!isStaff(interaction)) return;
+    await interaction.channel.delete();
+  }
+};
