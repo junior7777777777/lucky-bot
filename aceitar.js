905ba8bcbbf26d556f3aa65c0ea129e5fb4446f0
junior7777777777 <fazopix1214@gmail.com>
2026-01-15 22:49:06 -0300

    Refactor aceitar.js to fix formatting issues

--- aceitar.js
+++ aceitar.js
@@ -5,7 +5,6 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('aceitar')
     .setDescription('Aceitar participante'),
-
   async execute(interaction) {
     if (!isStaff(interaction)) return;
     await interaction.channel.delete();
